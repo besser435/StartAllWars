c76b0dda80c5951aabca9c17131cbae61aa7f1ab
besser <68145204+besser435@users.noreply.github.com>
2026-01-15 14:14:14 -0700

    Initial commit

---
+++ src/main/java/org/diet/StartAllWars.java
@@ -0,0 +1,17 @@
+package org.diet;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StartAllWars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
